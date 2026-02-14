abc3669af18f3addb58b59496e35a26a296fe854
Yeongseok Lim <ylim76@wisc.edu>
2026-02-14 17:43:36 -0600

    Backup before ui testing

---
+++ ios/ClearSplit/Sources/ClearSplit/Views/Components/Layout/TabLayoutMetrics.swift
@@ -0,0 +1,8 @@
+import SwiftUI
+
+enum TabLayoutMetrics {
+    static let horizontalPadding: CGFloat = 16
+    static let sectionSpacing: CGFloat = 16
+    static let topPadding: CGFloat = 12
+    static let bottomPaddingForTabBar: CGFloat = 104
+}
